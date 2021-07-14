36d82962c5e5143fc0d4ead388edda5b2f019f0f
詹家宇 <307344430@qq.com>
2021-07-14 20:22:35 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;/*
+ * @Author: {{author}}
+ * @Date:   {{create_time}}
+ * @Last Modified by:   {{last_modified_by}}
+ * @Last Modified time: {{last_modified_time}}
+ */
+
+public class Person {
+
+    private String id;
+
+}
